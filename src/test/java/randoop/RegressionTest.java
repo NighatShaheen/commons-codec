5e1f309acf4fdf98777fb6694f27491f5ad84ab0
NighatShaheen <nighat2414@gmail.com>
2025-02-02 21:09:26 +0100

    rerunning all workflows

---
+++ src/test/java/randoop/RegressionTest.java
@@ -0,0 +1,11 @@
+package randoop;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.JUnit4;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ RegressionTest0.class, RegressionTest1.class })
+public class RegressionTest {
+}
+
