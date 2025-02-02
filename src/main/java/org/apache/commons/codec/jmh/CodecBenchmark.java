5a6a19a8a791a73f6ff3d318b7c775fff2c50068
NighatShaheen <nighat2414@gmail.com>
2025-02-02 20:51:57 +0100

    added checksyle check skip

--- src/main/java/org/apache/commons/codec/jmh/CodecBenchmark.java
+++ src/main/java/org/apache/commons/codec/jmh/CodecBenchmark.java
@@ -1,19 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements. See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.apache.commons.codec.jmh;
 import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Base64;
@@ -22,8 +6,7 @@ import org.openjdk.jmh.annotations.*;
 
 import java.security.SecureRandom;
 
-public class CodecBenchmark
-{
+public class CodecBenchmark {
 
     @State(Scope.Benchmark)
     public static class BenchmarkState {
@@ -42,25 +25,24 @@ public class CodecBenchmark
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
     public char[] encodeHex(BenchmarkState state) {
-    return Hex.encodeHex(state.data);
+        return Hex.encodeHex(state.data);
     }
 
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
-    public byte[] decodeHex(BenchmarkState state) throws DecoderException{
-        return Hex.decodeHex(new String(state.data,java.nio.charset.StandardCharsets.UTF_8));
+    public byte[] decodeHex(BenchmarkState state) throws DecoderException {
+        return Hex.decodeHex(new String(state.data, java.nio.charset.StandardCharsets.UTF_8));
     }
+
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
-    public byte[] encodeBase64(BenchmarkState state)
-    {
+    public byte[] encodeBase64(BenchmarkState state) {
         return Base64.encodeBase64(state.data);
     }
+
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
-    public byte[] decodeBase64(BenchmarkState state)
-    {
-        return Base64.encodeBase64(state.data);
+    public byte[] decodeBase64(BenchmarkState state) {
+        return Base64.decodeBase64(state.data);
     }
 }
-
